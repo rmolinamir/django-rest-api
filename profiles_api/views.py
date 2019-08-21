1a21594db1f4cd7127044bafa1c93fb5e34156ce
Robert Molina <rmolinamir@gmail.com>
2019-08-21 12:27:22 -0400

    added profiles search filters

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet, ModelViewSet
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.filters import SearchFilter
 from rest_framework import status
 
 # Local
@@ -101,3 +102,5 @@ class UserProfileViewSet(ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (SearchFilter,)
+    search_fields = ('name', 'email',)
